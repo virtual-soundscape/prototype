fba1d961adb319a6097770e94bc5bc27e1b9163c
Anson Miu <me@ansonmiu.dev>
2021-10-22 21:57:30 +0100

    Frontend: fix issue with userId not always set, and removing avatar when someone disconnects

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Room.css'
 import { useParams } from "react-router";
+import { useEffect, useRef, useState } from "react";
 import Map from "../components/Map";
 import VideoGallery from "../components/VideoGallery";
 import io from 'socket.io-client';
@@ -8,18 +9,45 @@ import io from 'socket.io-client';
 
 function Room() {
   const { id } = useParams();
-  const socket = io.connect("http://localhost:8080")
-  socket.emit("newUser", id)
-  
+  const [userId, setUserId] = useState();
+
+  // Use a ref here, because we don't need the socket to be part of component
+  // state.
+
+  const socketRef = useRef();
+
+  // Only try connect when component mounts.
+
+  useEffect(() => {
+    const socket = io.connect("http://localhost:8080");
+
+    // Attach receive handler before sending the 'newUser' message, so we know
+    // how to handle the response.
+    // TBH, this should be done via HTTP request, since it's request/response.
+
+    socket.on("local", payload => {
+      setUserId(payload);
+    });
+    socket.emit("newUser", id);
+
+    socketRef.current = socket;
+  }, []);
+
+  const webSocketIsReady = userId !== undefined && socketRef.current;
+
+  // TODO: maybe show a loading screen if the WebSocket connection isn't ready.
 
     return (
       <div id="container" className="container-fluid">
         <div className="row">
           <div className="col-10">
-            <Map socket={socket} roomId={id}/>
+            {webSocketIsReady &&
+              <Map socket={socketRef.current} roomId={id} userId={userId} />}
           </div>
           <div className="col-2">
-            <VideoGallery socket={socket} roomId={id} />
+            {webSocketIsReady &&
+              <VideoGallery socket={socketRef.current} roomId={id} />
+            }
           </div>
         </div>
       </div>
