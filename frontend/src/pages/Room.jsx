efefcf14c10b474329d7faaf818dfd59eac082fd
GabeLi69 <gkli@mit.edu>
2021-10-23 18:07:24 +0800

    users object is now available in the Room component

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -10,6 +10,7 @@ import io from 'socket.io-client';
 function Room() {
   const { id } = useParams();
   const [userId, setUserId] = useState();
+  const [users, setUsers] = useState();
 
   // Use a ref here, because we don't need the socket to be part of component
   // state.
@@ -35,6 +36,10 @@ function Room() {
 
   const webSocketIsReady = userId !== undefined && socketRef.current;
 
+  const collectUsers = (users) => {
+    setUsers(users);
+  }
+
   // TODO: maybe show a loading screen if the WebSocket connection isn't ready.
 
     return (
@@ -42,11 +47,11 @@ function Room() {
         <div className="row">
           <div className="col-10">
             {webSocketIsReady &&
-              <Map socket={socketRef.current} roomId={id} userId={userId} />}
+              <Map socket={socketRef.current} roomId={id} userId={userId} collectUsers={(users) => collectUsers(users)} />}
           </div>
           <div className="col-2">
             {webSocketIsReady &&
-              <VideoGallery socket={socketRef.current} roomId={id} />
+              <VideoGallery socket={socketRef.current} roomId={id} users={users} />
             }
           </div>
         </div>
